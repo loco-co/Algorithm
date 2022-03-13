b232cde51b11dd01946ca0aa3ea196da53111866
Giseok Hwang <loco2925@gmail.com>
2022-03-13 17:57:23 +0900

    Create 더 맵게.py

---
+++ 코딩테스트 고득점 Kit/힙(Heap)/더 맵게.py
@@ -0,0 +1,11 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        if len(scoville) < 2 or scoville[0] == 0: return -1
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville)*2)
+        answer += 1
+    return answer
