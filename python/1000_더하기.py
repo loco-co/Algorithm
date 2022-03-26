2d161319dd5f9bc1c8040c66103e412cbcc2e4e3
Giseok Hwang <loco2925@gmail.com>
2022-03-26 14:44:01 +0900

    Delete python directory

--- python/1000_더하기.py
+++
@@ -1,4 +0,0 @@
-import sys
-input=sys.stdin.readline
-a=input().split()
-print(int(a[0])+int(a[1]))
--- python/2438_별찍기.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-n = int(sys.stdin.readline())
-for i in range(1, n+1):
-    for j in range(0, i):
-        print("*", end='')
-    print("")
